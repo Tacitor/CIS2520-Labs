677c80bb19e8d8f296324711385db41702bcf1d9
Lukas Krampitz <lkrampit@uoguelph.ca>
2023-09-20 12:47:34 -0400

    Finish the lab assignment to regular specs

--- lab2_main.c
+++ lab2_main.c
@@ -117,11 +117,96 @@ int main(int argc, char **argv)
 	int wordExtractorMaxLength = DEFAULT_WORD_EXTRACTOR_MAX_LENGTH;
 	int filesProcessed = 0;
 
-	// Change this to be your own code.
-	//
-	// Take a look at processWordsInFile() to actually do the
-	// work -- it is defined above.
-	printf("I don't know what to do!  Please help me!\n");
+	// Self declared variables
+	int forceUppercase = 0;
+	int forceLowercase = 0;
+	int wordLengthToPrint = DEFAULT_PRINT_LENGTH;
+
+	// loop thorugh the arguments
+	// NOTE: Much of this code was copied and adapted from my submission for A1
+	for (int i = 1; i < argc; i++)
+	{
+		// check to see if the arg starts with a "-"
+		if (argv[i][0] == '-')
+		{
+			// check for the next char of the flag
+			if (argv[i][1] == 'h')
+			{
+				// Help
+				printHelp();
+
+				// don't run the rest of the program
+				exit(1);
+			}
+			else if (argv[i][1] == 'L')
+			{
+				// Lower case
+				forceLowercase = 1;
+			}
+			else if (argv[i][1] == 'U')
+			{
+				// Upper case
+				forceUppercase = 1;
+			}
+			else if (argv[i][1] == 'l')
+			{
+				// length of word to print
+				// error check the next arg
+				for (int j = 0; argv[i + 1][j] != '\0'; j++)
+				{
+					// test for number
+					if (!isdigit(argv[i + 1][j]))
+					{
+						printf("Bad argument for -l\n");
+						return -1; // exit(-1)
+					}
+				}
+				wordLengthToPrint = atoi(argv[i + 1]);
+				// increment to the next arg so it doesn't get processed as a file
+				i++;
+			}
+			else if (argv[i][1] == 'W')
+			{
+				// maximum word size
+				// error check the next arg
+				for (int j = 0; argv[i + 1][j] != '\0'; j++)
+				{
+					// test for number
+					if (!isdigit(argv[i + 1][j]))
+					{
+						printf("Bad argument for -W\n");
+						return -1; // exit(-1)
+					}
+				}
+				wordExtractorMaxLength = atoi(argv[i + 1]);
+				// increment to the next arg so it doesn't get processed as a file
+				i++;
+			}
+		}
+		else
+		{
+			// file to process
+
+			// Take a look at processWordsInFile() to actually do the
+			// work -- it is defined above.
+			processWordsInFile(NULL,
+							   argv[i],
+							   wordExtractorMaxLength,
+							   wordLengthToPrint,
+							   forceUppercase,
+							   forceLowercase);
+
+			// count the file
+			filesProcessed++;
+		}
+	}
+
+	// debug the acceptance of command line args
+	// printf("wordExtractorMaxLength: %d\n", wordExtractorMaxLength);
+	// printf("filesProcessed: %d\n", filesProcessed);
+	// printf("forceUppercase: %d\n", forceUppercase);
+	// printf("forceLowercase: %d\n", forceLowercase);
+	// printf("wordLengthToPrint: %d\n", wordLengthToPrint);
 
 	if (filesProcessed == 0)
 	{
